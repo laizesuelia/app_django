af37403ba49bea94ddea36f29bc37be84b4e29d4
Laíze Suelia da Silva Oliveira <20261016270038@ifrn.local>
2026-06-03 20:22:41 -0300

    Criação do projeto django

---
+++ meuapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def contato(request):
+    return render(request, 'contato.html')
+
+
+# Create your views here.
