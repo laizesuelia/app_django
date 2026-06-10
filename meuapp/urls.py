69d140064929b9043e36597970dd7f2c6874d6e7
Laíze Suelia da Silva Oliveira <20261016270038@ifrn.local>
2026-06-10 19:25:14 -0300

    Criação do projeto django

--- meuapp/urls.py
+++ meuapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('sobre/', views.sobre, name='sobre'),
-    path('contato/', views.contato, name='contato'),
+    path("", views.index, name="index"),
+    path("sobre/", views.sobre, name="sobre"),
+    path("contato/", views.contato, name="contato"),
+    path("aluno/<int:id_aluno>", views.aluno, name="aluno"),
 ]
--- meuapp/views.py
+++ meuapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
+    print(request.method)
+    nome = request.GET.get("nome")
+    contexto = {
+        "nome": nome
+    }
     return render(request, 'index.html')
 
 def sobre(request):
@@ -9,5 +14,10 @@ def sobre(request):
 def contato(request):
     return render(request, 'contato.html')
 
+def aluno(request, id_aluno):
+    contexto = {
+        "id_aluno": id_aluno,
+    }
+    return render(request, "aluno.html", contexto)
 
 # Create your views here.
