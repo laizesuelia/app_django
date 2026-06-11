892f4d4b1cb0141ba60174b7b6c42f08922d59e0
Laíze Suelia da Silva Oliveira <20261016270038@ifrn.local>
2026-06-11 20:11:27 -0300

    Criação do projeto django

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("", include("meuapp.urls")),
-    path("catalogo/", include("catalogo.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
+    path("<int:id>/", views.detalhes, name="detalhes")
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -2,21 +2,43 @@ from django.shortcuts import render
 
 # Create your views here.
 
+obras = [
+    {
+        "id": 1,
+        "titulo": "Interestelar",
+        "tipo": "Filme",
+        "ano": 2014,
+        "genero": "ficcao",
+        "descricao": "Astronauta viajando a Lua",
+    },
+    {
+        "id": 2,
+        "titulo": "GoT",
+        "tipo": "Série",
+        "ano": 2014,
+        "genero": "ação",
+        "descricao": "Familias brigando por tronos",
+    }
+]
+
+
 def index(request):
-    obras = [
-        {
-            "id": 1, 
-            "titulo": "Interestelar",
-            "tipo": "Filme",
-        },
-        {
-            
-            "id": 2, 
-            "titulo": "GoT",
-            "tipo": "Série",
-        }
-    ]
     contexto = {
         "obras": obras,
     }
     return render(request, "catalogo/index.html", contexto)
+
+
+def detalhes(request, id):
+    obra_encontrada = None
+
+    for obra in obras:
+        if obra["id"] == id:
+            obra_encontrada = obra
+            break
+
+    contexto = {
+        "obra": obra_encontrada,
+    }
+
+    return render(request, "catalogo/detalhes.html", contexto)
